752689e4643d860bf100e249ad1d37e91e6432df
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-10-15 19:34:05 +0800

    fixed main function

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "mergeSort.h"
+#include "insertionSort.h"
 
 int main()
 {
-    int size, *array;
+    int size, *array, option;
     printf("Enter array size: \n");
     scanf("%d", &size);
     array = malloc(sizeof(int) * size);
@@ -12,11 +13,27 @@ int main()
     for (int i = 0; i < size; ++i) {
         scanf("%d", &array[i]);
     }
-    mergeSort(array, 0, size - 1);
+    printf("1. Merge sort\n");
+    printf("2. Insertion sort\n");
+    printf("Choose sorting:\n");
+    scanf("%d", &option);
+    switch (option){
+        case 1:
+            printf("Merge Sort\n");
+            mergeSort(array, 0 , size - 1);
+            break;
+        case 2:
+            printf("Insertion sort\n");
+            insertionSort(array, size);
+            break;
+        default:
+            printf("Not supported\n");
+    }
     printf("Sorted array: \n");
     for (int j = 0; j < size; ++j) {
         printf("%d ", array[j]);
     }
+    printf("\n");
 
     return 0;
 }
